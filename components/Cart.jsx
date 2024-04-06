b6edcb01905aaeec1f2fe04e9e2ce1cdf1830945
Ketan Singh <singhketan507@gmail.com>
2024-04-07 02:47:43 +0530

    added empty cart section

--- components/Cart.jsx
+++ components/Cart.jsx
@@ -11,10 +11,10 @@ import { FaCartShopping } from "react-icons/fa6";
 import { Separator } from "./ui/separator";
 import { formatPrice } from "@/lib/utils";
 import Link from "next/link";
-import { Button, buttonVariants } from "./ui/button";
+import Image from "next/image";
 
 const Cart = () => {
-  const itemCount = 1;
+  const itemCount = 0;
   const fee = 1;
   return (
     <Sheet>
@@ -59,7 +59,19 @@ const Cart = () => {
             </div>
           </>
         ) : (
-          <div></div>
+          <div className="flex h-full flex-col items-center justify-center space-y-1">
+            <div className="relative mb-4 h-60 w-60 text-muted-foreground">
+              <Image src="/bottlefinal.png" fill alt="empty shopping cart " />
+            </div>
+            <div className="text-xl font-semibold">Your cart is empty</div>
+            <SheetTrigger asChild>
+              <Link href="/shop">
+                <button className="w-full bg-[#1B3C87] px-[1.5rem] py-[.5rem] rounded-[.5rem] text-white font-semibold items-center justify-center">
+                  Add items to your cart to checkout
+                </button>
+              </Link>
+            </SheetTrigger>
+          </div>
         )}
       </SheetContent>
     </Sheet>
