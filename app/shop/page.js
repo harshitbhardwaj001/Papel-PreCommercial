01e524df7c2a78bd2f100742c5734f0cf8c4d86b
Harshit <harshitbhardwaj001@gmail.com>
2024-04-06 23:55:29 +0530

    Fixed Product Page

--- app/shop/page.js
+++ app/shop/page.js
@@ -6,7 +6,9 @@ import React from "react";
 const page = () => {
   return (
     <div>
-      <Navbar />
+      <div className="ml-[6%]">
+        <Navbar />
+      </div>
       <Products />
       <Footer />
     </div>
