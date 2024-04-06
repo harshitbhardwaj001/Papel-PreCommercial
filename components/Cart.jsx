4bfe1b8566d5220a16e6294dec684ab81533dc72
Ketan Singh <singhketan507@gmail.com>
2024-04-07 02:58:59 +0530

    changed image size for empty cart section

--- components/Cart.jsx
+++ components/Cart.jsx
@@ -60,7 +60,7 @@ const Cart = () => {
           </>
         ) : (
           <div className="flex h-full flex-col items-center justify-center space-y-1">
-            <div className="relative mb-4 h-60 w-60 text-muted-foreground">
+            <div className="relative mb-4 h-80 w-80 text-muted-foreground">
               <Image
                 src="/character-with-product-1.png"
                 fill
