75bd6c6f49e6a67f4ca89f5be2665622d6165afd
Harshit <harshitbhardwaj001@gmail.com>
2024-04-23 18:49:26 +0530

    Minor Fixes

--- app/shop/page.js
+++ app/shop/page.js
@@ -1,16 +1,11 @@
 "use client";
 
-import AboutPapel from "@/components/AboutPapel";
 import Footer from "@/components/Footer";
 import HeroBanner from "@/components/HeroBanner";
 import Navbar from "@/components/Navbar";
 import Products from "@/components/Products";
-import Slogan from "@/components/Slogan";
-import AboutUs from "@/components/AboutUs";
-import Videos from "@/components/Videos";
+
 import React from "react";
-import Testimonials from "@/components/Testimonials";
-import Subscription from "@/components/Subscription";
 
 const page = () => {
   return (
@@ -24,13 +19,6 @@ const page = () => {
       <div className="scroll-smooth" id="product">
         <Products />
       </div>
-      {/* <Videos />
-      <AboutPapel />
-      <Slogan />
-
-      <AboutUs />
-      <Testimonials />
-      <Subscription /> */}
 
       <Footer />
     </div>
